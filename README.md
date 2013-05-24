f18921713bb7fd999061df38a4f9e77034235e02
Chad Schulz <chad.awesome@gmail.com>
2013-05-23 21:44:24 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+## ReneeAndChadSchulz.com
---
+++ server.js
@@ -0,0 +1,25 @@
+var express      = require( 'express' )
+    ,http        = require( 'http' )
+    ,path        = require( 'path' )
+    ,app         = express();
+
+// Environment Settings
+app.set( 'port', process.env.PORT || 3001 );
+app.set( 'views', __dirname + '/views' );
+app.set( 'view engine', 'ejs' );
+
+// Middleware
+app.use( express.favicon() );
+app.use( express.bodyParser() );
+app.use( express.compress() );
+app.use( express.static( path.join( __dirname, 'public' ) ) );
+
+// Routing
+app.get( '/', function( req, res ){
+    res.render( 'index' );
+});
+
+// Create Server
+http.createServer( app ).listen( app.get( 'port' ), function( ){
+    console.log( 'Server listening on port ' + app.get( 'port' ) );
+});
